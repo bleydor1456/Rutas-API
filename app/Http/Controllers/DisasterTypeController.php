04ce809606bdaf2d9dc7877f9341b302e5748e99
bleydor1456 <sergio.licona.colorado@gmail.com>
2023-02-20 20:23:20 -0800

    hk

---
+++ app/Http/Controllers/DisasterTypeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DisasterType;
+use App\Http\Requests\DisasterTypeStoreRequest;
+
+class DisasterTypeController extends Controller
+{
+    public function index() {
+        try {
+            //Query all dame levels
+            $disaster_type = DisasterType::get();
+
+            //return json response
+            return response()->json($disaster_type);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function store(Request $request) {
+        try {
+            $data = $request->all();
+
+            //Query all dame levels
+            $disaster_type = DisasterType::create($data);
+
+            //return json response
+            return response()->json($disaster_type);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    public function show($id)
+    {
+        $m = DisasterType::find($id);
+
+        return response()->json($m);
+    }
+}
